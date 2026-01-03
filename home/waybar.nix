3aeea2a231266c23be2b12a2fa7a72675063f445
67 <support@linux.com>
2026-01-03 06:25:20 +0100

    feat: waybar styling

--- home/waybar.nix
+++ home/waybar.nix
@@ -1,431 +1,135 @@
-{ lib
-, config
-, pkgs
-, ...
-}:
-let
-  cfg = config.home-config.waybar;
-in
-{
-  options.home-config.waybar = {
-    enable = lib.mkOption {
-      default = pkgs.stdenv.isLinux;
-      type = lib.types.bool;
-    };
-    output = lib.mkOption {
-      default = null;
-      type = lib.types.listOf lib.types.str;
-    };
-    battery = lib.mkOption {
-      default = false;
-      type = lib.types.bool;
-    };
-  };
+{ ... }: {
 
-  config = lib.mkIf cfg.enable {
-    programs.waybar = {
-      enable = true;
-      systemd.enable = true;
+  programs.waybar = {
+    enable = true;
+    settings = [{
+        layer = "top";
+        position = "bottom";
+        spacing = 0;
+        height = 0;
 
-      settings = {
-        mainBar = {
-          layer = "top";
-          position = "top";
-          height = 30;
-          output = cfg.output;
-          modules-left = [
-            "hyprland/workspaces"
-          ];
-          modules-center = [
-            "hyprland/window"
-          ];
-          modules-right =
-            if cfg.battery then
-              [ "battery" ]
-            else
-              [ ]
-              ++ [
-                "tray"
-                "clock"
-              ];
+        margin-top = 0;
+        margin-right = 0;
+        margin-bottom = 0;
+        margin-left = 0;
 
-          "hyprland/workspaces" = {
-            disable-scroll = true;
-            all-outputs = false;
-          };
+        modules-left = [
+            "custom/grrr"
+            "niri/workspaces"
+            "tray"
+            "custom/notification"
+        ];
 
-          "battery" = {
-            "interval" = 60;
-            "states" = {
-              "warning" = 30;
-              "critical" = 1;
-            };
-            "format" = "{capacity}% {icon}";
-            "format-icons" = [
-              ""
-              ""
-              ""
-              ""
-              ""
-            ];
-            "max-length" = 25;
-          };
+        modules-center = [
+            "clock#secondary"
+            "clock"
+            "custom/weather"
+        ];
 
-          "clock" = {
-            "format" = "{:%H:%M}";
-            "format-alt" = "{:%A, %B %d, %Y (%R)}";
-            "tooltip-format" = "<tt><small>{calendar}</small></tt>";
-            "calendar" = {
-              "mode" = "year";
-              "mode-mon-col" = 3;
-              "weeks-pos" = "right";
-              "on-scroll" = 1;
-              "format" = {
-                "months" = "<span color='#ffead3'><b>{}</b></span>";
-                "days" = "<span color='#ecc6d9'><b>{}</b></span>";
-                "weeks" = "<span color='#99ffdd'><b>W{}</b></span>";
-                "weekdays" = "<span color='#ffcc66'><b>{}</b></span>";
-                "today" = "<span color='#ff6699'><b><u>{}</u></b></span>";
-              };
-            };
-            "actions" = {
-              "on-click-right" = "mode";
-              "on-scroll-up" = "shift_up";
-              "on-scroll-down" = "shift_down";
-            };
-          };
+        modules-right = [
+            "cpu"
+            "custom/temp"
+            "memory"
+            "network"
+            "pulseaudio"
+            "battery"
+        ];
+
+        "sway/workspaces" = {
+            "disable-scroll" = true;
+            "all-outputs" = true;
+            "tooltip" = false;
         };
-      };
 
-      style = ''
-        * {
-            /* `otf-font-awesome` is required to be installed for icons */
-            font-family: FontAwesome, Roboto, Helvetica, Arial, sans-serif;
-            font-size: 13px;
-        }
+        tray = {
+            "show-passive-items" = true;
+            "spacing" = 10;
+            "tooltip" = false;
+        };
 
-        window#waybar {
-            background-color: rgba(43, 48, 59, 0.5);
-            border-bottom: 3px solid rgba(100, 114, 125, 0.5);
-            color: #ffffff;
-            transition-property: background-color;
-            transition-duration: .5s;
-        }
+        clock = {
+            "format" = "  {:%I:%M:%S %p}";
+            "interval" = 1;
+            "tooltip" = false;
+        };
 
-        window#waybar.hidden {
-            opacity: 0.2;
-        }
+        "clock#secondary" = {
+            "format" = "  {:%a, %d %b %Y}";
+            "interval" = 1;
+            "tooltip" = false;
+        };
 
-        /*
-        window#waybar.empty {
-            background-color: transparent;
-        }
-        window#waybar.solo {
-            background-color: #FFFFFF;
-        }
-        */
+        cpu = {
+            "format" = "  {usage}%";
+            "interval" = 1;
 
-        window#waybar.termite {
-            background-color: #3F3F3F;
-        }
+            states = {
+                "critical" = 90;
+            };
+        };
 
-        window#waybar.chromium {
-            background-color: #000000;
-            border: none;
-        }
+        memory = {
+            "format" = "  {percentage}%";
+            "interval" = 2;
 
-        button {
-            /* Use box-shadow instead of border so the text isn't offset */
-            box-shadow: inset 0 -3px transparent;
-            /* Avoid rounded borders under each button name */
-            border: none;
-            border-radius: 0;
-        }
+            states = {
+                "critical" = 80;
+            };
+        };
 
-        /* https://github.com/Alexays/Waybar/wiki/FAQ#the-workspace-buttons-have-a-strange-hover-effect */
-        button:hover {
-            background: inherit;
-            box-shadow: inset 0 -3px #ffffff;
-        }
+        network = {
+            "format-wifi" = "  {bandwidthDownBytes} {bandwidthUpBytes}";
+            "format-ethernet" = "  {bandwidthDownBytes} {bandwidthUpBytes}";
+            "format-disconnected" = "󱚼  no network";
+            "interval" = 1;
+            "tooltip" = false;
+        };
 
-        /* you can set a style on hover for any module like this */
-        #pulseaudio:hover {
-            background-color: #a37800;
-        }
+        pulseaudio = {
+            "scroll-step" = 5;
+            "max-volume" = 150;
+            "format" = "  {volume}%";
+            "format-bluetooth" = "  {volume}%";
+            "nospacing" = 1;
+            "on-click" = "pavucontrol";
+            "tooltip" = false;
+        };
 
-        #workspaces button {
-            padding: 0 5px;
-            background-color: transparent;
-            color: #ffffff;
-        }
+        battery = {
+            "bat" = "BAT0";
+            "format" = "  {capacity}%";
+        };
 
-        #workspaces button:hover {
-            background: rgba(0, 0, 0, 0.2);
-        }
+        "custom/notification" = {
+            "tooltip" = false;
+            "format" = "{icon}";
+            "format-icons" = {
+                "notification" = "<span foreground='#d65d0e'><sup></sup></span>";
+                "none" = "";
+                "dnd-notification" = "󰂛<span foreground='#d65d0e'><sup></sup></span>";
+                "dnd-none" = "󰂛";
+                "inhibited-notification" = "<span foreground='#d65d0e'><sup></sup></span>";
+                "inhibited-none" = "";
+                "dnd-inhibited-notification" = "󰂛<span foreground='#d65d0e'><sup></sup></span>";
+                "dnd-inhibited-none" = "󰂛";
+            };
+            "return-type" = "json";
+            "exec-if" = "which swaync-client";
+            "exec" = "swaync-client -swb";
+            "on-click" = "swaync-client -t -sw";
+            "on-click-right" = "swaync-client -d -sw";
+            "escape" = true;
+        };
 
-        #workspaces button.focused {
-            background-color: #64727D;
-            box-shadow: inset 0 -3px #ffffff;
-        }
+        "custom/grrr" = {
+            "exec" = "bash print_grrr.sh";
+        };
 
-        #workspaces button.urgent {
-            background-color: #eb4d4b;
-        }
-
-        #mode {
-            background-color: #64727D;
-            box-shadow: inset 0 -3px #ffffff;
-        }
-
-        #clock,
-        #battery,
-        #cpu,
-        #memory,
-        #disk,
-        #temperature,
-        #backlight,
-        #network,
-        #pulseaudio,
-        #wireplumber,
-        #custom-media,
-        #tray,
-        #mode,
-        #idle_inhibitor,
-        #scratchpad,
-        #power-profiles-daemon,
-        #mpd {
-            padding: 0 10px;
-            color: #ffffff;
-        }
-
-        #window,
-        #workspaces {
-            margin: 0 4px;
-        }
-
-        /* If workspaces is the leftmost module, omit left margin */
-        .modules-left > widget:first-child > #workspaces {
-            margin-left: 0;
-        }
-
-        /* If workspaces is the rightmost module, omit right margin */
-        .modules-right > widget:last-child > #workspaces {
-            margin-right: 0;
-        }
-
-        #clock {
-            background-color: #64727D;
-        }
-
-        #battery {
-            background-color: #ffffff;
-            color: #000000;
-        }
-
-        #battery.charging, #battery.plugged {
-            color: #ffffff;
-            background-color: #26A65B;
-        }
-
-        @keyframes blink {
-            to {
-                background-color: #ffffff;
-                color: #000000;
-            }
-        }
-
-        /* Using steps() instead of linear as a timing function to limit cpu usage */
-        #battery.critical:not(.charging) {
-            background-color: #f53c3c;
-            color: #ffffff;
-            animation-name: blink;
-            animation-duration: 0.5s;
-            animation-timing-function: steps(12);
-            animation-iteration-count: infinite;
-            animation-direction: alternate;
-        }
-
-        #power-profiles-daemon {
-            padding-right: 15px;
-        }
-
-        #power-profiles-daemon.performance {
-            background-color: #f53c3c;
-            color: #ffffff;
-        }
-
-        #power-profiles-daemon.balanced {
-            background-color: #2980b9;
-            color: #ffffff;
-        }
-
-        #power-profiles-daemon.power-saver {
-            background-color: #2ecc71;
-            color: #000000;
-        }
-
-        label:focus {
-            background-color: #000000;
-        }
-
-        #cpu {
-            background-color: #2ecc71;
-            color: #000000;
-        }
-
-        #memory {
-            background-color: #9b59b6;
-        }
-
-        #disk {
-            background-color: #964B00;
-        }
-
-        #backlight {
-            background-color: #90b1b1;
-        }
-
-        #network {
-            background-color: #2980b9;
-        }
-
-        #network.disconnected {
-            background-color: #f53c3c;
-        }
-
-        #pulseaudio {
-            background-color: #f1c40f;
-            color: #000000;
-        }
-
-        #pulseaudio.muted {
-            background-color: #90b1b1;
-            color: #2a5c45;
-        }
-
-        #wireplumber {
-            background-color: #fff0f5;
-            color: #000000;
-        }
-
-        #wireplumber.muted {
-            background-color: #f53c3c;
-        }
-
-        #custom-media {
-            background-color: #66cc99;
-            color: #2a5c45;
-            min-width: 100px;
-        }
-
-        #custom-media.custom-spotify {
-            background-color: #66cc99;
-        }
-
-        #custom-media.custom-vlc {
-            background-color: #ffa000;
-        }
-
-        #temperature {
-            background-color: #f0932b;
-        }
-
-        #temperature.critical {
-            background-color: #eb4d4b;
-        }
-
-        #tray {
-            background-color: #2980b9;
-        }
-
-        #tray > .passive {
-            -gtk-icon-effect: dim;
-        }
-
-        #tray > .needs-attention {
-            -gtk-icon-effect: highlight;
-            background-color: #eb4d4b;
-        }
-
-        #idle_inhibitor {
-            background-color: #2d3436;
-        }
-
-        #idle_inhibitor.activated {
-            background-color: #ecf0f1;
-            color: #2d3436;
-        }
-
-        #mpd {
-            background-color: #66cc99;
-            color: #2a5c45;
-        }
-
-        #mpd.disconnected {
-            background-color: #f53c3c;
-        }
-
-        #mpd.stopped {
-            background-color: #90b1b1;
-        }
-
-        #mpd.paused {
-            background-color: #51a37a;
-        }
-
-        #language {
-            background: #00b093;
-            color: #740864;
-            padding: 0 5px;
-            margin: 0 5px;
-            min-width: 16px;
-        }
-
-        #keyboard-state {
-            background: #97e1ad;
-            color: #000000;
-            padding: 0 0px;
-            margin: 0 5px;
-            min-width: 16px;
-        }
-
-        #keyboard-state > label {
-            padding: 0 5px;
-        }
-
-        #keyboard-state > label.locked {
-            background: rgba(0, 0, 0, 0.2);
-        }
-
-        #scratchpad {
-            background: rgba(0, 0, 0, 0.2);
-        }
-
-        #scratchpad.empty {
-        	background-color: transparent;
-        }
-
-        #privacy {
-            padding: 0;
-        }
-
-        #privacy-item {
-            padding: 0 5px;
-            color: white;
-        }
-
-        #privacy-item.screenshare {
-            background-color: #cf5700;
-        }
-
-        #privacy-item.audio-in {
-            background-color: #1ca000;
-        }
-
-        #privacy-item.audio-out {
-            background-color: #0069d4;
-        }
-      '';
-    };
+        "custom/weather" = {
+            "exec" = "curl -s 'wttr.in/Bielefeld?format=1'";
+            "interval" = 600;
+        };
+    }];
   };
 }
+
