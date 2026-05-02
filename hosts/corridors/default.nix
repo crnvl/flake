adfa40e32c1b7fd4cea8f325dc78c8dc76e434c2
67 <support@linux.com>
2026-05-02 20:34:17 +0200

    corridors: refactor services config

--- hosts/corridors/default.nix
+++ hosts/corridors/default.nix
@@ -14,15 +14,17 @@
 
   hardware.graphics.enable = true;
 
-  services.greetd = {
-    enable = true;
-    settings = {
-      default_session = {
-        command = "niri-session";
-        user = "aleph";
+  services = {
+    greetd = {
+      enable = true;
+      settings = {
+        default_session = {
+          command = "niri-session";
+          user = "aleph";
+        };
       };
     };
-  };
 
-  services.xserver.videoDrivers = [ "modesetting" ];
+    xserver.videoDrivers = [ "modesetting" ];
+  };
 }
