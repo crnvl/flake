5850ef678baa57057c16518eb812a3efcc3b504a
67 <support@linux.com>
2026-05-02 20:02:37 +0200

    corridors: format hw config

--- hosts/corridors/hardware-configuration.nix
+++ hosts/corridors/hardware-configuration.nix
@@ -1,13 +1,25 @@
-{ config, lib, pkgs, modulesPath, ... }:
+{
+  config,
+  lib,
+  modulesPath,
+  ...
+}:
 
 {
-  imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
-    ];
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
 
   boot = {
     initrd = {
-      availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" "sr_mod" ];
+      availableKernelModules = [
+        "xhci_pci"
+        "ahci"
+        "usb_storage"
+        "usbhid"
+        "sd_mod"
+        "sr_mod"
+      ];
       kernelModules = [ ];
     };
 
@@ -16,26 +28,29 @@
   };
 
   fileSystems = {
-    "/" =
-      { device = "/dev/disk/by-uuid/8b128acd-261d-4b41-9ee1-dc7437ddbcd1";
-        fsType = "ext4";
-      };
+    "/" = {
+      device = "/dev/disk/by-uuid/8b128acd-261d-4b41-9ee1-dc7437ddbcd1";
+      fsType = "ext4";
+    };
 
-    "/boot" =
-      { device = "/dev/disk/by-uuid/DD14-D330";
-        fsType = "vfat";
-        options = [ "fmask=0022" "dmask=0022" ];
-      };
+    "/boot" = {
+      device = "/dev/disk/by-uuid/DD14-D330";
+      fsType = "vfat";
+      options = [
+        "fmask=0022"
+        "dmask=0022"
+      ];
+    };
 
-    "/mnt/storage1" =
-      { device = "/dev/disk/by-uuid/39dc6076-8b89-46f2-af13-89d6fcdb6740";
-        fsType = "ext4";
-      };
+    "/mnt/storage1" = {
+      device = "/dev/disk/by-uuid/39dc6076-8b89-46f2-af13-89d6fcdb6740";
+      fsType = "ext4";
+    };
 
-    "/mnt/storage2" =
-      { device = "/dev/disk/by-uuid/a149898d-99d8-4d56-983a-abce9a48ce81";
-        fsType = "ext4";
-      };
+    "/mnt/storage2" = {
+      device = "/dev/disk/by-uuid/a149898d-99d8-4d56-983a-abce9a48ce81";
+      fsType = "ext4";
+    };
   };
 
   swapDevices = [ ];
