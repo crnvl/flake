f9a99587a0c5ec3ca5e84080400333989ff188cf
67 <support@linux.com>
2026-05-03 15:42:38 +0200

    shimmers: secrets

--- secrets.nix
+++ secrets.nix
@@ -1,7 +1,6 @@
 let
   shimmers = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOs1x+98r+MXYAitek6+yrO96TnyOWoZ9IO6MPONqcmY";
 
-  # Your user keys
   aleph-chambers = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH++Jm6a+gQf5yEdTzT5ozuIQdkYb2w98UxsX2I1YJlg";
   aleph-corridors = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIXBwAlJ0BxHk01MZ5QnVHbmS5tgO+Rubg0MyJsIk5dp";
   aleph-macbook = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICbVo7Bj9I6oZc8cM4IPDNhF6MSns8X8S8HrVhql/PxV";
