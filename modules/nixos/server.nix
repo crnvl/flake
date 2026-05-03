bb89e479390eca21ee13458caec58746bf4acddf
67 <support@linux.com>
2026-05-03 12:09:59 +0200

    server: add root user

--- modules/nixos/server.nix
+++ modules/nixos/server.nix
@@ -1,5 +1,12 @@
 { ... }:
 
+let
+  sshKeys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIXBwAlJ0BxHk01MZ5QnVHbmS5tgO+Rubg0MyJsIk5dp" # corridors
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH++Jm6a+gQf5yEdTzT5ozuIQdkYb2w98UxsX2I1YJlg" # chambers
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICbVo7Bj9I6oZc8cM4IPDNhF6MSns8X8S8HrVhql/PxV" # macbook
+  ];
+in
 {
   services.openssh = {
     enable = true;
@@ -14,9 +21,6 @@
     allowedTCPPorts = [ 22 ];
   };
 
-  users.users.aleph.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIXBwAlJ0BxHk01MZ5QnVHbmS5tgO+Rubg0MyJsIk5dp" # corridors
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH++Jm6a+gQf5yEdTzT5ozuIQdkYb2w98UxsX2I1YJlg" # chambers
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICbVo7Bj9I6oZc8cM4IPDNhF6MSns8X8S8HrVhql/PxV" # macbook
-  ];
+  users.users.aleph.openssh.authorizedKeys.keys = sshKeys;
+  users.users.root.openssh.authorizedKeys.keys = sshKeys;
 }
