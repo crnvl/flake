b002b5e50ece21746eab14fe21cf2f1f6922ae4e
67 <support@linux.com>
2026-05-03 20:23:40 +0200

    transmission: create dirs

--- modules/nixos/services/transmission.nix
+++ modules/nixos/services/transmission.nix
@@ -25,63 +25,70 @@ in
 
   boot.kernelModules = [ "wireguard" ];
 
-  systemd.services = {
-    transmission = {
-      serviceConfig.NetworkNamespacePath = "/run/netns/mullvad";
-      after = [ "mullvad-netns.service" ];
-      requires = [ "mullvad-netns.service" ];
-    };
-
-    mullvad-netns = {
-      description = "Mullvad WireGuard VPN namespace";
-      wantedBy = [ "multi-user.target" ];
-      before = [ "transmission.service" ];
-      wants = [ "network-online.target" ];
-      after = [ "network-online.target" ];
-
-      serviceConfig = {
-        Type = "oneshot";
-        RemainAfterExit = true;
-        ExecStop = "${pkgs.iproute2}/bin/ip netns del mullvad";
+  systemd = {
+    services = {
+      transmission = {
+        serviceConfig.NetworkNamespacePath = "/run/netns/mullvad";
+        after = [ "mullvad-netns.service" ];
+        requires = [ "mullvad-netns.service" ];
       };
 
-      path = [
-        pkgs.iproute2
-        pkgs.wireguard-tools
+      mullvad-netns = {
+        description = "Mullvad WireGuard VPN namespace";
+        wantedBy = [ "multi-user.target" ];
+        before = [ "transmission.service" ];
+        wants = [ "network-online.target" ];
+        after = [ "network-online.target" ];
+
+        serviceConfig = {
+          Type = "oneshot";
+          RemainAfterExit = true;
+          ExecStop = "${pkgs.iproute2}/bin/ip netns del mullvad";
+        };
+
+        path = [
+          pkgs.iproute2
+          pkgs.wireguard-tools
+        ];
+
+        script = ''
+          # Create namespace
+          ip netns add mullvad || true
+          ip -n mullvad link set lo up
+
+          # Create WireGuard interface in namespace
+          ip link add wg0 type wireguard
+          ip link set wg0 netns mullvad
+
+          # Configure WireGuard (inside namespace since wg0 is there now)
+          ip netns exec mullvad wg set wg0 \
+            private-key ${config.age.secrets.mullvad-wg-private-key.path} \
+            peer ${mullvadPeerKey} \
+            endpoint ${mullvadEndpoint} \
+            allowed-ips 0.0.0.0/0,::0/0
+
+          ip -n mullvad addr add ${mullvadAddress} dev wg0
+          ip -n mullvad link set wg0 up
+          ip -n mullvad route add default dev wg0
+
+          # DNS inside namespace
+          mkdir -p /etc/netns/mullvad
+          echo "nameserver ${mullvadDns}" > /etc/netns/mullvad/resolv.conf
+
+          # Port forward: host:9091 -> namespace:9091 (for Sonarr/Radarr)
+          ip link add veth-host type veth peer name veth-mullvad
+          ip link set veth-mullvad netns mullvad
+          ip addr add 10.200.0.1/24 dev veth-host
+          ip -n mullvad addr add 10.200.0.2/24 dev veth-mullvad
+          ip link set veth-host up
+          ip -n mullvad link set veth-mullvad up
+        '';
+      };
+
+      tmpfiles.rules = [
+        "d /var/lib/transmission/downloads 0755 transmission transmission -"
+        "d /var/lib/transmission/incomplete 0755 transmission transmission -"
       ];
-
-      script = ''
-        # Create namespace
-        ip netns add mullvad || true
-        ip -n mullvad link set lo up
-
-        # Create WireGuard interface in namespace
-        ip link add wg0 type wireguard
-        ip link set wg0 netns mullvad
-
-        # Configure WireGuard (inside namespace since wg0 is there now)
-        ip netns exec mullvad wg set wg0 \
-          private-key ${config.age.secrets.mullvad-wg-private-key.path} \
-          peer ${mullvadPeerKey} \
-          endpoint ${mullvadEndpoint} \
-          allowed-ips 0.0.0.0/0,::0/0
-
-        ip -n mullvad addr add ${mullvadAddress} dev wg0
-        ip -n mullvad link set wg0 up
-        ip -n mullvad route add default dev wg0
-
-        # DNS inside namespace
-        mkdir -p /etc/netns/mullvad
-        echo "nameserver ${mullvadDns}" > /etc/netns/mullvad/resolv.conf
-
-        # Port forward: host:9091 -> namespace:9091 (for Sonarr/Radarr)
-        ip link add veth-host type veth peer name veth-mullvad
-        ip link set veth-mullvad netns mullvad
-        ip addr add 10.200.0.1/24 dev veth-host
-        ip -n mullvad addr add 10.200.0.2/24 dev veth-mullvad
-        ip link set veth-host up
-        ip -n mullvad link set veth-mullvad up
-      '';
     };
   };
 }
