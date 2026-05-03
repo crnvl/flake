753cec31150ad7af4291cd544d18943bf00cc0b5
67 <support@linux.com>
2026-05-03 14:01:27 +0200

    kanidm: use new config structure

--- modules/nixos/services/kanidm.nix
+++ modules/nixos/services/kanidm.nix
@@ -3,13 +3,16 @@
 {
   services.kanidm = {
     package = pkgs.kanidm_1_9;
-    enable = true;
-    serverSettings = {
-      origin = "https://id.shimme.rs";
-      domain = "id.shimme.rs";
-      bindaddress = "127.0.0.1:8443";
-      tls_chain = "/var/lib/acme/id.shimme.rs/fullchain.pem";
-      tls_key = "/var/lib/acme/id.shimme.rs/key.pem";
+
+    server = {
+      enable = true;
+      settings = {
+        origin = "https://id.shimme.rs";
+        domain = "id.shimme.rs";
+        bindaddress = "127.0.0.1:8443";
+        tls_chain = "/var/lib/acme/id.shimme.rs/fullchain.pem";
+        tls_key = "/var/lib/acme/id.shimme.rs/key.pem";
+      };
     };
   };
 
