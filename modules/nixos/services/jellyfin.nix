1d7907985c25ff982de50e5e793b873c47f97034
67 <support@linux.com>
2026-05-03 16:08:28 +0200

    jellyfin: set forwarded header

--- modules/nixos/services/jellyfin.nix
+++ modules/nixos/services/jellyfin.nix
@@ -13,6 +13,10 @@
     locations."/" = {
       proxyPass = "http://127.0.0.1:8096";
       proxyWebsockets = true;
+
+      extraConfig = ''
+        proxy_set_header X-Forwarded-Proto https;
+      '';
     };
   };
 }
