b4054734a8d2c6b61213846e0ddd9891ce3cb522
67 <support@linux.com>
2026-05-03 16:14:55 +0200

    waybar: remove 0 margins

--- modules/home/waybar.nix
+++ modules/home/waybar.nix
@@ -11,11 +11,6 @@
         spacing = 0;
         height = 0;
 
-        margin-top = 0;
-        margin-right = 0;
-        margin-bottom = 0;
-        margin-left = 0;
-
         modules-left = [
           "niri/workspaces"
           "tray"
