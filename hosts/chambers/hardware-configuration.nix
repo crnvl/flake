9ed6b4c64c7fd053c5ab84d3e0ceb1d7634efb4d
67 <support@linux.com>
2026-05-11 17:56:03 +0200

    chambers: enable bt

--- hosts/chambers/hardware-configuration.nix
+++ hosts/chambers/hardware-configuration.nix
@@ -45,5 +45,11 @@
   ];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware = {
+    bluetooth = {
+      enable = true;
+      powerOnBoot = false;
+    };
+    cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  };
 }
