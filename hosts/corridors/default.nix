36fde647859d0dea023402a63edd345e697c37ef
67 <support@linux.com>
2026-05-19 18:58:27 +0200

    corridors: firewall

--- hosts/corridors/default.nix
+++ hosts/corridors/default.nix
@@ -10,6 +10,17 @@
   networking = {
     hostName = "corridors";
     networkmanager.enable = true;
+
+    firewall = {
+      allowedTCPPorts = [
+        27036
+        27037
+      ];
+      allowedUDPPorts = [
+        27031
+        27036
+      ];
+    };
   };
 
   programs.steam = {
