19be88405699285d10ad720bd97a8b57e2ecef68
67 <support@linux.com>
2026-06-24 21:41:15 +0200

    backup: use borg

--- modules/nixos/backup.nix
+++ modules/nixos/backup.nix
@@ -15,35 +15,38 @@
       BatchMode yes
   '';
 
-  services.restic.backups.home = {
-    initialize = true;
-    repository = "sftp:backup-box:/backups/${config.networking.hostName}";
-    passwordFile = "/etc/restic-home-password";
-
+  services.borgbackup.jobs.home = {
     paths = [ "/home/aleph" ];
+    repo = "ssh://u615907@u615907.your-storagebox.de:23/./backups/${config.networking.hostName}";
 
-    extraBackupArgs = [
-      "--exclude=/home/aleph/.cache"
-      "--exclude=/home/aleph/.android/avd"
-      "--exclude=/home/aleph/Android"
-      "--exclude=/home/aleph/.local/share/Steam"
-      "--exclude=/home/aleph/.vagrant.d/boxes"
-      "--exclude=/home/aleph/.gradle"
-      "--exclude=**/node_modules"
-      "--exclude=**/target"
-      "--exclude=**/__pycache__"
-    ];
+    environment.BORG_RSH = "ssh -i /home/aleph/.ssh/id_ed25519 -o StrictHostKeyChecking=accept-new -o BatchMode=yes -o ProxyCommand='ssh -i /home/aleph/.ssh/id_ed25519 -o StrictHostKeyChecking=accept-new -W %h:%p aleph@shimme.rs'";
 
-    timerConfig = {
-      OnCalendar = "daily";
-      Persistent = true;
-      RandomizedDelaySec = "30min";
+    encryption = {
+      mode = "repokey-blake2";
+      passCommand = "cat /etc/borg-passphrase";
     };
 
-    pruneOpts = [
-      "--keep-daily 7"
-      "--keep-weekly 4"
-      "--keep-monthly 6"
+    compression = "auto,zstd";
+    startAt = "daily";
+
+    prune.keep = {
+      daily = 7;
+      weekly = 4;
+      monthly = 6;
+    };
+
+    exclude = [
+      "pp:/home/aleph/.cache"
+      "pp:/home/aleph/.android/avd"
+      "pp:/home/aleph/Android"
+      "pp:/home/aleph/.local/share/Steam"
+      "pp:/home/aleph/.vagrant.d/boxes"
+      "pp:/home/aleph/.gradle"
+      "sh:**/node_modules"
+      "sh:**/target"
+      "sh:**/__pycache__"
     ];
+
+    doInit = true;
   };
 }
