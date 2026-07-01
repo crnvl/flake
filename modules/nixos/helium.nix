8771fb953f06b73f072907642708d1e3e68a1631
67 <support@linux.com>
2026-07-01 18:59:48 +0200

    helium: extensions

--- modules/nixos/helium.nix
+++ modules/nixos/helium.nix
@@ -14,6 +14,18 @@
 
       ShowHomeButton = true;
       BookmarkBarEnabled = true;
+
+      ExtensionInstallForcelist = [
+        "nngceckbapebfimnlniiiahkandclblb;https://clients2.google.com/service/update2/crx" # Bitwarden
+        "cdglnehniifkbagbbombnjghhcihifij;https://clients2.google.com/service/update2/crx" # Kagi Search
+        "mendokngpagmkejfpmeellpppjgbpdaj;https://clients2.google.com/service/update2/crx" # Kagi Privacy Pass
+      ];
+
+      DefaultSearchProviderEnabled = true;
+      DefaultSearchProviderName = "Kagi";
+      DefaultSearchProviderKeyword = "kagi";
+      DefaultSearchProviderSearchURL = "https://kagi.com/search?q={searchTerms}";
+      DefaultSearchProviderSuggestURL = "https://kagisuggest.com/api/autosuggest?q={searchTerms}";
     };
   };
 }
