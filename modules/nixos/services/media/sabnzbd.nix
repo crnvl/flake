ea8337660f2b49daa8a9e9bed60ce38ea503a280
67 <support@linux.com>
2026-07-09 22:23:16 +0200

    sabnzbd: redundant parent

--- modules/nixos/services/media/sabnzbd.nix
+++ modules/nixos/services/media/sabnzbd.nix
@@ -5,9 +5,8 @@
     enable = true;
   };
 
-  systemd.services.sabnzbd.serviceConfig.ExecStart = lib.mkForce (
-    "${lib.getBin pkgs.sabnzbd}/bin/sabnzbd -d -f /var/lib/sabnzbd/sabnzbd.ini -s 0.0.0.0:8080 -b 0"
-  );
+  systemd.services.sabnzbd.serviceConfig.ExecStart =
+    lib.mkForce "${lib.getBin pkgs.sabnzbd}/bin/sabnzbd -d -f /var/lib/sabnzbd/sabnzbd.ini -s 0.0.0.0:8080 -b 0";
 
   systemd.services.sabnzbd.vpnConfinement = {
     enable = true;
