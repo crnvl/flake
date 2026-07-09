4f82a2ff79f23b7529806b4405b4b64e75191a7b
67 <support@linux.com>
2026-07-09 17:25:03 +0200

    flake: refactor secrets

--- secrets.nix
+++ secrets.nix
@@ -2,17 +2,24 @@ let
   keys = import ./keys.nix;
   users = builtins.attrValues keys.users;
   shimmers = keys.hosts.shimmers;
+
+  secrets = [
+    "kanidm-idm-admin-password"
+    "kanidm-admin-password"
+    "kanidm-oauth2-jellyfin-secret"
+    "kanidm-oauth2-catshift-secret"
+    "kanidm-oauth2-vaultwarden-secret"
+    "mullvad-wg.conf"
+    "caelo-env"
+    "chroma-smb"
+    "radarr-api-key"
+    "vaultwarden-env"
+    "vaultwarden-borg-passphrase"
+  ];
 in
-{
-  "hosts/shimmers/secrets/kanidm-idm-admin-password.age".publicKeys = users ++ [ shimmers ];
-  "hosts/shimmers/secrets/kanidm-admin-password.age".publicKeys = users ++ [ shimmers ];
-  "hosts/shimmers/secrets/kanidm-oauth2-jellyfin-secret.age".publicKeys = users ++ [ shimmers ];
-  "hosts/shimmers/secrets/kanidm-oauth2-catshift-secret.age".publicKeys = users ++ [ shimmers ];
-  "hosts/shimmers/secrets/kanidm-oauth2-vaultwarden-secret.age".publicKeys = users ++ [ shimmers ];
-  "hosts/shimmers/secrets/mullvad-wg.conf.age".publicKeys = users ++ [ shimmers ];
-  "hosts/shimmers/secrets/caelo-env.age".publicKeys = users ++ [ shimmers ];
-  "hosts/shimmers/secrets/chroma-smb.age".publicKeys = users ++ [ shimmers ];
-  "hosts/shimmers/secrets/radarr-api-key.age".publicKeys = users ++ [ shimmers ];
-  "hosts/shimmers/secrets/vaultwarden-env.age".publicKeys = users ++ [ shimmers ];
-  "hosts/shimmers/secrets/vaultwarden-borg-passphrase.age".publicKeys = users ++ [ shimmers ];
-}
+builtins.listToAttrs (
+  map (name: {
+    name = "hosts/shimmers/secrets/${name}.age";
+    value.publicKeys = users ++ [ shimmers ];
+  }) secrets
+)
