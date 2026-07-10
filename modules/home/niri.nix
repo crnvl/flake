e72c4242a20bd0344a8678531b2800d9a86a0b30
67 <support@linux.com>
2026-07-10 17:02:09 +0200

    niri: update shortcuts

--- modules/home/niri.nix
+++ modules/home/niri.nix
@@ -299,9 +299,9 @@ in
       "Mod+C".action.center-column = [ ];
       "Mod+Ctrl+C".action.center-visible-columns = [ ];
       "Mod+Minus".action.set-column-width = "-10%";
-      "Mod+Equal".action.set-column-width = "+10%";
+      "Mod+Plus".action.set-column-width = "+10%";
       "Mod+Shift+Minus".action.set-window-height = "-10%";
-      "Mod+Shift+Equal".action.set-window-height = "+10%";
+      "Mod+Shift+Plus".action.set-window-height = "+10%";
 
       # floating/tabbed
       "Mod+V".action.toggle-window-floating = [ ];
